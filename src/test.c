54989a14503d926b25e03ff4e5c7c208640a4753
Kalen Novis White <kalen@krworks.com>
2025-08-01 15:19:02 -0700

    convert headers to .sf

---
+++ src/test.c
@@ -0,0 +1,6 @@
+#include <import>
+
+// we dont need to alter our C file when silver generates the schema and headers; they all go in import
+none test_init(test a) {
+    print("test initialized");
+}
