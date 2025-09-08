9e90d67f70c75ea16b25d6e2388c9b475130f7bf
Kalen Novis White <kalen@krworks.com>
2025-09-08 09:37:37 -0700

    ...

--- src/test.c
+++ src/test.c
@@ -1,6 +1,6 @@
 #include <import>
 
 // we dont need to alter our C file when silver generates the schema and headers; they all go in import
-none test_init(test a) {
-    print("test initialized");
+none test_vulkan_init(test a) {
+    print("test_vulkan initialized");
 }
